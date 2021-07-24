e04d0036c5d8ac107f28dc1d60b4bdca4baaed5f
pauldjackson70 <86663755+pauldjackson70@users.noreply.github.com>
2021-07-24 13:51:25 -0500

    Add Main window selection buttons

--- mathgame/src/mathgame/app.py
+++ mathgame/src/mathgame/app.py
@@ -4,19 +4,43 @@ An application built with Beeware's Toga to test basic math
 import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
-
+import random
 
 class Mathgame(toga.App):
 
     def startup(self):
         """
         Construct and show the Toga application.
-
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
         """
-        main_box = toga.Box()
+        main_box = toga.Box(style=Pack(direction=ROW))
+        
+        def open_sequence(widget):
+            print('sequence')
+            pass
+
+        def open_addition(widget):
+            print('addition')
+            pass
+
+        btn_open_sequence = toga.Button(
+            'Sequence',
+            on_press=open_sequence,
+            style=Pack(
+                padding=5, 
+                font_size=16
+            )
+        )
+        btn_open_addition = toga.Button(
+            'Addition',
+            on_press=open_addition,
+            style=Pack(
+                padding=5, 
+                font_size=16
+            )
+        )
+
+        main_box.add(btn_open_sequence)
+        main_box.add(btn_open_addition)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
